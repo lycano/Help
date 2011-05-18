afd9db8abebff5c10e85dc1d4d8198d47dfc7387
Lycano <luricos@gmx.de>
2011-05-18 22:52:16 +0200

    get rid of groupmanager * changed version string in pom.xml to 0.3

--- src/main/java/me/taylorkelly/help/HelpPermissions.java
+++ src/main/java/me/taylorkelly/help/HelpPermissions.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import com.nijikokun.bukkit.Permissions.Permissions;
-import org.anjocaido.groupmanager.GroupManager;
 
 public class HelpPermissions {
     private enum PermissionHandler {
@@ -16,15 +15,9 @@ public class HelpPermissions {
     private static Plugin permissionPlugin;
 
     public static void initialize(Server server) {
-        Plugin groupManager = server.getPluginManager().getPlugin("GroupManager");
         Plugin permissions = server.getPluginManager().getPlugin("Permissions");
-
-        if (groupManager != null) {
-            permissionPlugin = groupManager;
-            handler = PermissionHandler.GROUP_MANAGER;
-            String version = groupManager.getDescription().getVersion();
-            HelpLogger.info("Permissions enabled using: GroupManager v" + version);
-        } else if (permissions != null) {
+        
+        if (permissions != null) {
             permissionPlugin = permissions;
             handler = PermissionHandler.PERMISSIONS;
             String version = permissions.getDescription().getVersion();
@@ -39,8 +32,6 @@ public class HelpPermissions {
         switch (handler) {
             case PERMISSIONS:
                 return ((Permissions)permissionPlugin).getHandler().permission(player, permission);
-            case GROUP_MANAGER:
-                return ((GroupManager)permissionPlugin).getWorldsHolder().getWorldPermissions(player).has(player, permission);
             case NONE:
                 return true;
             default:
